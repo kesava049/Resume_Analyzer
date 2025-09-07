5514ff9ec216bbf274d7fabd682cd66f191a5c01
kalepallikesavulareddy@gmail.com <kalepallikesavulareddy@gmail.com>
2025-09-07 17:57:40 +0530

    resumesAndScreenshots_

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require('cors');
 
-// 1. Import your new router
+
 const resumeRoutes = require('./api/resumes/resume.router');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 2. Mount the router
+
 // Tell Express that any request to "/api/resume" should be handled by resumeRoutes
 app.use('/api/resume', resumeRoutes);
 
